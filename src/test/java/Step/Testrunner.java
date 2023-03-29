9dbc59a3719618d79e3d527cee029b104ede4439
arunbalasundar <64471661+arunbalasundar@users.noreply.github.com>
2023-03-30 09:34:38 +1300

    Update Testrunner.java
    removing tags to run all cases

--- src/test/java/Step/Testrunner.java
+++ src/test/java/Step/Testrunner.java
@@ -7,7 +7,6 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features",glue= {"Step"},
-tags="",
 plugin = {"pretty","junit:target/JUnitReports/report.xml",		
 		"json:target/JSONReports/report.json",
 		"html:target/HtmlReport/report.html"},monochrome = true)
